bcc940a399d2f4ee92ac24651ca711e1ed43b8e6
Naya-Jevan Goodman <ngoodman@ucvts.tec.nj.us>
2020-01-16 13:59:32 -0500

    inital commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -4,7 +4,7 @@ public class Exercises {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
-		
+		//oof
 		// write your code here
 		
 		return false;	// default return value to ensure compilation
